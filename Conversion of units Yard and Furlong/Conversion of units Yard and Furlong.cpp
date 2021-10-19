029b182d1ea458a8d194aac65e0fdf066a900cb1
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-19 15:54:48 +0300

    red

---
+++ Conversion of units Yard and Furlong/Conversion of units Yard and Furlong.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double furlong, yard;
+    cout << "Enter count of furlongs: " << endl;
+        cin >> furlong;
+        yard = furlong * 220;
+        cout << "There is " << yard << " yards in furlongs" << endl;
+}
